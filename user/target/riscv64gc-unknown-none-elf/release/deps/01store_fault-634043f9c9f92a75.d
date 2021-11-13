db3a20fef49c500814e005b6a9f200ed89653a45
zzzzzhang989 <804232899@qq.com>
2021-11-13 11:49:34 +0800

    设计和实现应用程序

---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/01store_fault-634043f9c9f92a75.d
@@ -0,0 +1,5 @@
+/root/Operating-system-experiment/user/target/riscv64gc-unknown-none-elf/release/deps/01store_fault-634043f9c9f92a75: src/bin/01store_fault.rs
+
+/root/Operating-system-experiment/user/target/riscv64gc-unknown-none-elf/release/deps/01store_fault-634043f9c9f92a75.d: src/bin/01store_fault.rs
+
+src/bin/01store_fault.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/release/deps/02power-52821bb9303416e6.d
@@ -0,0 +1,5 @@
+/root/Operating-system-experiment/user/target/riscv64gc-unknown-none-elf/release/deps/02power-52821bb9303416e6: src/bin/02power.rs
+
+/root/Operating-system-experiment/user/target/riscv64gc-unknown-none-elf/release/deps/02power-52821bb9303416e6.d: src/bin/02power.rs
+
+src/bin/02power.rs:
