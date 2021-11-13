3953b8bf02d47f84a1622b2ba7eb28b642e2e24f
zzzzzhang989 <804232899@qq.com>
2021-11-13 11:13:45 +0800

    裸机环境和最小化内核

---
+++ target/riscv64gc-unknown-none-elf/release/deps/os-411ec35b302d4858.d
@@ -0,0 +1,5 @@
+/root/Operating-system-experiment/target/riscv64gc-unknown-none-elf/release/deps/os-411ec35b302d4858: src/main.rs
+
+/root/Operating-system-experiment/target/riscv64gc-unknown-none-elf/release/deps/os-411ec35b302d4858.d: src/main.rs
+
+src/main.rs:
---
+++ target/riscv64gc-unknown-none-elf/release/os.d
@@ -0,0 +1,1 @@
+/root/Operating-system-experiment/target/riscv64gc-unknown-none-elf/release/os: /root/Operating-system-experiment/src/main.rs
