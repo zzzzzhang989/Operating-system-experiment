11c93c2cb911d68ebf776e9d407306a8363ea29b
zhangchi <804232899@qq.com>
2021-11-14 18:36:03 +0800

    内存管理debug4

--- user/src/syscall.rs
+++ user/src/syscall.rs
@@ -7,24 +7,27 @@ fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
     unsafe {
         asm!("ecall",
-             in("x10") args[0],
+             inlateout("x10") args[0] => ret,
              in("x11") args[1],
              in("x12") args[2],
              in("x17") id,
-             lateout("x10") ret
         );
     }
     ret
 }
+
 pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
     syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
 }
+
 pub fn sys_exit(exit_code: i32) -> isize {
     syscall(SYSCALL_EXIT, [exit_code as usize, 0, 0])
 }
+
 pub fn sys_yield() -> isize {
     syscall(SYSCALL_YIELD, [0, 0, 0])
 }
+
 pub fn sys_get_time() -> isize {
     syscall(SYSCALL_GET_TIME, [0, 0, 0])
 }
