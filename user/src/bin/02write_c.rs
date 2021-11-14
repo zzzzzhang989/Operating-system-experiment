eec5678b8601f6c919852c3f8894562fbd21c527
zzzzzhang989 <804232899@qq.com>
2021-11-14 10:30:51 +0800

    多道程序与协作式调度debug

--- user/src/bin/02write_c.rs
+++ user/src/bin/02write_c.rs
@@ -1,14 +1,10 @@
 #![no_std]
 #![no_main]
-
 #[macro_use]
 extern crate user_lib;
-
 use user_lib::yield_;
-
 const WIDTH: usize = 10;
 const HEIGHT: usize = 5;
-
 #[no_mangle]
 fn main() -> i32 {
     for i in 0..HEIGHT {
@@ -16,6 +12,6 @@ fn main() -> i32 {
         println!(" [{}/{}]", i + 1, HEIGHT);
         yield_();
     }
-    println!("Test write_a OK!");
+    println!("Test write_c OK!");
     0
 }
